0a7e95d8eb3d5ca0868e6f6be88fe174537f78d8
Will Asbury <asburywg@gmail.com>
2022-03-01 21:35:33 -0500

    cache

--- rarity/collections.py
+++ rarity/collections.py
@@ -1,4 +1,6 @@
+import datetime
 import inspect
+import json
 from dataclasses import dataclass, field
 from typing import Optional, Dict
 import requests
@@ -15,93 +17,103 @@ https://github.com/Quantumlyy/rarity-tools-scraper-py
 @dataclass_json
 @dataclass
 class ProjectLookup:
-	name: str
-	added_order: Optional[int] = field(metadata=dt_json_config(field_name="addedOrder"), default=None)
-	dynamic: Optional[int] = None
-	manual: Optional[bool] = None
-	contracts: Optional[list[str]] = None
-	cv: Optional[int] = None
-	item_name: Optional[str] = field(metadata=dt_json_config(field_name="itemName"), default=None)
-	added: Optional[str] = None
-	custom_header: Optional[str] = field(metadata=dt_json_config(field_name="customHeader"), default=None)
+    name: str
+    added_order: Optional[int] = field(metadata=dt_json_config(field_name="addedOrder"), default=None)
+    dynamic: Optional[int] = None
+    manual: Optional[bool] = None
+    contracts: Optional[list[str]] = None
+    cv: Optional[int] = None
+    item_name: Optional[str] = field(metadata=dt_json_config(field_name="itemName"), default=None)
+    added: Optional[str] = None
+    custom_header: Optional[str] = field(metadata=dt_json_config(field_name="customHeader"), default=None)
 
 
 @dataclass_json
 @dataclass
 class Projects:
-	list: list[str]
-	lookup: Dict[str, ProjectLookup]
+    list: list[str]
+    lookup: Dict[str, ProjectLookup]
 
 
 @dataclass_json
 @dataclass
 class CollectionStats:
-	one_day_volume: Optional[float] = None
-	one_day_change: Optional[float] = None
-	one_day_sales: Optional[float] = None
-	one_day_average_price: Optional[float] = None
-	seven_day_volume: Optional[float] = None
-	seven_day_change: Optional[float] = None
-	seven_day_sales: Optional[float] = None
-	seven_day_average_price: Optional[float] = None
-	total_volume: Optional[float] = None
-	total_sales: Optional[float] = None
-	total_supply: Optional[float] = None
-	num_owners: Optional[float] = None
-	average_price: Optional[float] = None
-	market_cap: Optional[float] = None
-	floor_price: Optional[float] = None
+    one_day_volume: Optional[float] = None
+    one_day_change: Optional[float] = None
+    one_day_sales: Optional[float] = None
+    one_day_average_price: Optional[float] = None
+    seven_day_volume: Optional[float] = None
+    seven_day_change: Optional[float] = None
+    seven_day_sales: Optional[float] = None
+    seven_day_average_price: Optional[float] = None
+    total_volume: Optional[float] = None
+    total_sales: Optional[float] = None
+    total_supply: Optional[float] = None
+    num_owners: Optional[float] = None
+    average_price: Optional[float] = None
+    market_cap: Optional[float] = None
+    floor_price: Optional[float] = None
 
 
 @dataclass_json
 @dataclass
 class Collection:
-	id: str
-	slug: str
-	name: str
-	description: str
-	created_date: Optional[str] = None
-	discord_url: Optional[str] = None
-	external_url: Optional[str] = None
-	image_url: Optional[str] = None
-	large_image_url: Optional[str] = None
-	banner_image_url: Optional[str] = None
-	medium_username: Optional[str] = None
-	twitter_username: Optional[str] = None
-	instagram_username: Optional[str] = None
+    id: str
+    slug: str
+    name: str
+    description: str
+    created_date: Optional[str] = None
+    discord_url: Optional[str] = None
+    external_url: Optional[str] = None
+    image_url: Optional[str] = None
+    large_image_url: Optional[str] = None
+    banner_image_url: Optional[str] = None
+    medium_username: Optional[str] = None
+    twitter_username: Optional[str] = None
+    instagram_username: Optional[str] = None
 
-	stats: Optional[CollectionStats] = None
+    stats: Optional[CollectionStats] = None
 
 
 @dataclass_json
 @dataclass
 class Collections:
-	collections: list[Collection]
-	projects: Projects
-	unknown_things: CatchAll = None
+    collections: list[Collection]
+    projects: Projects
+    unknown_things: CatchAll = None
 
-	def sort_by_seven_day_volume(self, prop: str) -> list[Collection]:
-		prop_list = inspect.getmembers(CollectionStats)[0][1].keys()
-		if prop not in prop_list:
-			raise ValueError("Stat property options: %r." % prop_list)
-		has_stats = [i for i in self.collections if i.stats]
-		return sorted(has_stats, key=lambda x: x.stats.__dict__.get(prop), reverse=True)
-
-
-def get_all_collections() -> Collections:
-	res = requests.get(COLLECTIONS_ENDPOINT_URL)
-	json = res.json()
-
-	collections = Collections.from_dict(json)
-
-	for i in range(len(json["collections"])):
-		collections.collections[i] = Collection.from_dict(json["collections"][i])
-
-	return collections
+    def sort_by_seven_day_volume(self, prop: str) -> list[Collection]:
+        prop_list = inspect.getmembers(CollectionStats)[0][1].keys()
+        if prop not in prop_list:
+            raise ValueError("Stat property options: %r." % prop_list)
+        has_stats = [i for i in self.collections if i.stats]
+        return sorted(has_stats, key=lambda x: x.stats.__dict__.get(prop), reverse=True)
 
 
-def get_collections_df():
-	res = requests.get(COLLECTIONS_ENDPOINT_URL)
-	json = res.json()
-	return pd.json_normalize(json['collections'])
+class CollectionsManager:
+
+    def __init__(self, auto_cache=True, dir_path="../cache"):
+        self.dir_path = dir_path
+        self.collections = self.fetch_rarity_collections()
+        if auto_cache:
+            self.cache()
+
+    @staticmethod
+    def fetch_rarity_collections() -> dict:
+        res = requests.get(COLLECTIONS_ENDPOINT_URL)
+        return res.json()
+
+    def get_collections_objects(self) -> Collections:
+        collections = Collections.from_dict(self.collections)
+        for i in range(len(self.collections["collections"])):
+            collections.collections[i] = Collection.from_dict(self.collections["collections"][i])
+        return collections
+
+    def get_collections_df(self):
+        return pd.json_normalize(self.collections['collections'])
+
+    def cache(self):
+        dt = str(datetime.date.today()).replace("-", "")
+        with open(f"{self.dir_path}/rarity-{dt}.json", 'w') as f:
+            json.dump(self.collections, f)
 
--- rarity/rarity_tools.py
+++ rarity/rarity_tools.py
@@ -1,12 +1,15 @@
-from rarity import collections
 import pandas as pd
+from rarity.collections import CollectionsManager
 
 pd.set_option('display.max_columns', None)
 
-# c = collections.get_all_collections()
-# top = c.sort_by_seven_day_volume("seven_day_volume")[:10]
+
+c = CollectionsManager()
+
+# collections = c.get_collections_objects()
+# top = collections.sort_by_seven_day_volume("seven_day_volume")[:10]
 # for t in top:
 #     print(t)
 
-c = collections.get_collections_df()
-print(c.sort_values('stats.seven_day_volume', ascending=False))
+# collections = c.get_collections_df()
+# print(collections.sort_values('stats.seven_day_volume', ascending=False))
